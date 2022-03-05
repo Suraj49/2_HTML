646d27c16782b93dd831408a173d646d20741a24
Suraj49 <1507surajkumar@gmail.com>
2022-03-05 10:40:48 +0530

    commit

---
+++ function.js
@@ -0,0 +1,21 @@
+function sayhello(){
+    console.log("HELLO")
+}
+sayhello()
+function add(a,b){
+    console.log("the sum is",a+b)
+}
+add(4,5)
+function sub(a,b){
+    console.log("Sub is",a-b)
+
+}
+sub(6,3)
+function mul(a,b){
+    console.log("mul is",a*b)
+}
+mul(6,3)
+function div(a,b){
+    console.log("div is ",a/b)
+}
+div(9,3)
