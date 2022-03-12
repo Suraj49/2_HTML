99e2f6203d6cdbcc7131358b659473193049f626
Suraj49 <1507surajkumar@gmail.com>
2022-03-12 16:14:27 +0530

    commit

---
+++ 1_Object.js
@@ -0,0 +1,31 @@
+let obj = {}//declere 
+let person = {
+    name: 'Suraj',
+    age: 29,
+    phone: 12347583,
+    gender: 'male',
+    height: '6 feet'
+}
+
+let car={
+    name : 'Thar',
+    brand : 'Mahindra',
+    topSpeed : '155 Kmph',
+    price : '13.17 lakh',
+    color : 'Red'
+}
+let suraj={
+    name:'Suraj',
+    lastname:'kumar',
+    age : 22,
+    height : 6,
+    address:{
+        city:'Muzaffarpur',
+        state:'Bihar'
+    },
+
+    sayHi:function(){
+        console.log("Hello Suraj")
+    }
+
+}
