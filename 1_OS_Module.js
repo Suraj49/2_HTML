95bda7e555d762c34aaeefa79f074d159b9cb58b
Suraj49 <1507surajkumar@gmail.com>
2022-05-05 10:15:21 +0530

    commits

---
+++ 1_OS_Module.js
@@ -0,0 +1,9 @@
+const os=require('os')
+
+console.log(os.arch())  //this prints your system architechture
+
+console.log(os.platform()) //this shows system platform
+
+console.log(os.networkInterfaces()) //network inforomation
+
+console.log(os.cpus()) //gives details about your cpu
