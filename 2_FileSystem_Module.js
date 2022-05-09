122e4302fdfc26a58b26c2cc04cd5a90dfa6ed28
Suraj49 <1507surajkumar@gmail.com>
2022-05-09 17:05:27 +0530

    commite

--- 2_FileSystem_Module.js
+++ 2_FileSystem_Module.js
@@ -5,13 +5,14 @@
 const fileSystem=require('fs')
 
 //create myDirectory
+
 fileSystem.mkdirSync('myDirectory')
 console.log('myDirectory Created')
 
 // //delete myDirectory
+
 fileSystem.rmdirSync('myDirectory')
 console.log('myDirectory deleted')
-
 
 //Check 
 // ture or false
@@ -30,3 +31,8 @@ console.log('is File ?',Statistics.isFile())
 
 console.log('is Directory ?',Statistics.isDirectory())
 
+//ReadDirSync
+
+let folderPath=  'D:\\Dev\\2_HTML\\myDirectory'
+let folderCont=fileSystem.readdirSync(folderPath)
+console.log('folder Content -> '+folderCont)
